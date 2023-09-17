d5393199eb96d8b01fc22c5f32efaa8587974802
Fink Eduard <124546977+Sovanno@users.noreply.github.com>
2023-09-17 13:28:21 +0600

    Create PROGRAMMING_Task_1.py

---
+++ PROGRAMMING_Task_1.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+def conclusion():
+    print("--------------Task 1--------------")
+    print(task1())
+    print("--------------Task 2--------------")
+    print(task2(my_array))
+    print("--------------Task 3--------------")
+    print(task3(A))
+    print("--------------Task 4--------------")
+    print(task4())
+    print("--------------Task 5--------------")
+    print(task5(A1, B))
+    print("--------------Task 6--------------")
+    print(task6(A1, B))
+    print("--------------Task 7--------------")
+    print(task7(A1, B))
+    print("--------------Task 8--------------")
+    print(task8(A2, B1))
+    print("--------------Task 9--------------")
+    print(task9(A2, B1))
+    print("--------------Task 10--------------")
+    print(task10())
+
+def task1():
+    return np.arange(10, 70, 2)
+
+my_array = task1()
+
+def task2(my_array):
+    return np.reshape(my_array, (6, 5)).T
+
+A = task2(my_array)
+
+def task3(A):
+    A1 = A * 2.5
+    A1[0, :] -= 5
+    return A1
+
+A1 = task3(A)
+
+def task4():
+    return np.random.randint(0, 11, (6, 3))
+
+B = task4()
+
+def task5(A1, B):
+    a = [0] * len(A1)
+    for i in range(len(A1)):
+        for j in range(len(A1[0])):
+            a[i] = A1[i, j] + a[i]
+    b = [0] * len(B[0])
+    for i in range(len(B[0])):
+        for j in range(len(B)):
+            b[i] = B[j, i] + b[i]
+    return len(a), len(b)
+
+def task6(A1, B):
+    return np.dot(A1, B)
+
+def task7(A1, B):
+    A1 = np.delete(A1, 2, 1)
+    for i in range(3):
+        a = np.random.randint(10, 20, (6, 1))
+        B = np.c_[B, a]
+    return A1, B
+
+A2, B1 = task7(A1, B)
+
+def task8(A2, B1):
+    a_det = np.linalg.det(A2)
+    b_det = np.linalg.det(B1)
+    if a_det != 0:
+        a_inv = np.linalg.inv(A2)
+        print(a_inv)
+    else: print("a_inv does not exist")
+    if b_det != 0:
+        b_inv = np.linalg.inv(B1)
+        print(b_inv)
+    else: print("b_inv does not exist")
+    return a_det, b_det
+
+def task9(A2, B1):
+    A2 = np.linalg.matrix_power(A2, 6)
+    B1 = np.linalg.matrix_power(B1, 14)
+    return A2, B1
+
+def task10():
+    a = np.array([[2.7, 4, -3, 3], [2, -16, -9, 0], [8, -5, -3, -4], [2, 0, -1.7, -1.7]])
+    b = np.array([2, 8, 0, -4])
+    x = np.linalg.solve(a, b)
+    return x
+
+conclusion()
